b6bf8c4f9995c64fc3f407cdd3e99acdfff474fe
adonay <adona@DESKTOP-G3I7EKM>
2026-02-09 08:03:15 -0600

    Prueba 2

--- WindowsFormsApp1/Forms/FormLogin.Designer.cs
+++ WindowsFormsApp1/Forms/FormLogin.Designer.cs
@@ -36,9 +36,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(355, 170);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.Size = new System.Drawing.Size(47, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Prueba";
+            this.label1.Text = "Prueba2";
             // 
             // FormLogin
             // 
