cd099e3b7cec2d2e1b44703b61c207cad807471a
adonay <adona@DESKTOP-G3I7EKM>
2026-02-09 10:26:37 -0600

    avance del formInicio

--- WindowsFormsApp1/Forms/FormInicio.Designer.cs
+++ WindowsFormsApp1/Forms/FormInicio.Designer.cs
@@ -28,12 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panelSombra = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(67)))), ((int)(((byte)(87)))));
+            this.label1.Location = new System.Drawing.Point(3, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "¡Bienvenido de nuevo!";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(765, 55);
+            this.panel1.TabIndex = 1;
+            this.panel1.SizeChanged += new System.EventHandler(this.panel1_SizeChanged);
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.White;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 82);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(145, 356);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Location = new System.Drawing.Point(181, 82);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(596, 130);
+            this.panel2.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Location = new System.Drawing.Point(181, 231);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(596, 207);
+            this.panel3.TabIndex = 4;
+            // 
+            // panelSombra
+            // 
+            this.panelSombra.BackColor = System.Drawing.Color.Black;
+            this.panelSombra.Enabled = false;
+            this.panelSombra.Location = new System.Drawing.Point(12, 12);
+            this.panelSombra.Name = "panelSombra";
+            this.panelSombra.Size = new System.Drawing.Size(776, 64);
+            this.panelSombra.TabIndex = 5;
+            // 
+            // FormInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(67)))), ((int)(((byte)(87)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormInicio";
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelSombra);
+            this.Name = "FormInicio";
+            this.Text = "Daily Routine";
+            this.Load += new System.EventHandler(this.FormInicio_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panelSombra;
     }
 }
--- WindowsFormsApp1/Forms/FormInicio.cs
+++ WindowsFormsApp1/Forms/FormInicio.cs
@@ -10,11 +10,46 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1.Forms
 {
+    using System.Drawing.Drawing2D;
+
     public partial class FormInicio : Form
     {
         public FormInicio()
         {
+
             InitializeComponent();
+
+        }
+        private void RedondearPanel(Panel panel, int radio)
+        {
+            GraphicsPath path = new GraphicsPath();
+            path.StartFigure();
+
+            path.AddArc(0, 0, radio, radio, 180, 90);
+            path.AddArc(panel.Width - radio, 0, radio, radio, 270, 90);
+            path.AddArc(panel.Width - radio, panel.Height - radio, radio, radio, 0, 90);
+            path.AddArc(0, panel.Height - radio, radio, radio, 90, 90);
+
+            path.CloseFigure();
+            panel.Region = new Region(path);
+        }
+
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void FormInicio_Load(object sender, EventArgs e)
+        {
+            RedondearPanel(panel1, 20);
+            panelSombra.BackColor = Color.FromArgb(60, Color.Black);
+            RedondearPanel(panelSombra, 20);
+        }
+
+        private void panel1_SizeChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
