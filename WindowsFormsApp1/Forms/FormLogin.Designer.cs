c5c3992742fa920c3d07a0911e9ffe3daa593e1f
adonay <adona@DESKTOP-G3I7EKM>
2026-02-09 08:08:35 -0600

    Prueba de repositorio

--- WindowsFormsApp1/Forms/FormLogin.Designer.cs
+++ WindowsFormsApp1/Forms/FormLogin.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -37,25 +36,15 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(355, 170);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.Size = new System.Drawing.Size(29, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Prueba2";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(343, 196);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label1.Text = "SOS";
             // 
             // FormLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "FormLogin";
             this.Text = "Daily Routine";
@@ -67,7 +56,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
     }
 }
 
